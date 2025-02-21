4904cd2d73efcbb875c78bb30359ab0305386e3c
naratteu <skfhddlg@naver.com>
2025-02-21 22:07:44 +0900

    modernization - dotnet9, wwwroot, append, data:

--- src/Program.cs
+++ src/Program.cs
@@ -2,34 +2,24 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+using System.Net.Mime;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddCors(options
-        => options.AddPolicy(
-            name: "cors",
-            configurePolicy: policyBuilder =>
-            {
-                policyBuilder.AllowAnyHeader();
-                policyBuilder.AllowAnyMethod();
-                policyBuilder.AllowAnyOrigin();
-            }));
-
 await using var app = builder.Build();
 
-app.UseCors("cors");
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.MapGet(
     pattern: "/sse",
     requestDelegate: async context =>
     {
-        context.Response.Headers.Add("Content-Type", "text/event-stream");
+        context.Response.Headers.Append("Content-Type", MediaTypeNames.Text.EventStream);
 
         while (true)
         {
-            await context.Response.WriteAsync($"{DateTimeOffset.Now}\n");
+            await context.Response.WriteAsync($"data: {DateTimeOffset.Now}\n\n");
             await context.Response.Body.FlushAsync();
             await Task.Delay(500);
         }
